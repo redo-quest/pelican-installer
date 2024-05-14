bd8b4a07e32bd1387a8e98f552cbee24cf8f8bd2
v182 <142740981+v182@users.noreply.github.com>
2024-05-14 08:48:25 +0200

    install fix

--- install.sh
+++ install.sh
@@ -520,13 +520,15 @@ MONITOR_PID=$!
 cd /
 sudo add-apt-repository ppa:ondrej/php
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
-sudo apt install -y php8.2 php8.2-gd php8.2-mysql php8.2-mbstring php8.2-bcmath php8.2-xml php8.2-curl php8.2-zip php8.2-intl php8.2-sqlite3 php8.2-fpm php8.2-redis
+sudo apt install -y php8.2 php8.2-gd php8.2-mysql php8.2-mbstring php8.2-bcmath php8.2-xml php8.2-curl php8.2-zip php8.2-intl php8.2-sqlite3 php8.2-fpm php8.2-redis composer nginx
+Y
 mkdir -p /var/www/pelican
 cd /var/www/pelican
 curl -Lo panel.tar.gz https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz
 tar -xzvf panel.tar.gz
 chmod -R 755 storage/* bootstrap/cache/
 composer install --no-dev --optimize-autoloader
+yes
 php artisan p:environment:setup
 php artisan p:environment:database
 php artisan p:environment:mail
