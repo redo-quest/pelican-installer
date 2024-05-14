872364665a5882e9a9b05b7aa07c51cf773dd18a
v182 <142740981+v182@users.noreply.github.com>
2024-05-14 08:18:54 +0200

    push installation <3

--- install.sh
+++ install.sh
@@ -517,25 +517,23 @@ MONITOR_PID=$!
 
 
 {
-    bash <(curl -s https://pterodactyl-installer.se) <<EOF
-    0
-    $( [[ "$cpu_arch_conflict" == "true" ]] && echo "y" )
-    panel
-    pterodactyl
-    $database_password
-    Europe/Berlin
-    $admin_email
-    $admin_email
-    admin
-    Admin
-    User
-    $user_password
-    $panel_domain
-    N
-    N
-    N
-    y
-    yes
+cd /
+sudo add-apt-repository ppa:ondrej/php
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+sudo apt install -y php8.2 php8.2-gd php8.2-mysql php8.2-mbstring php8.2-bcmath php8.2-xml php8.2-curl php8.2-zip php8.2-intl php8.2-sqlite3 php8.2-fpm php8.2-redis
+mkdir -p /var/www/pelican
+cd /var/www/pelican
+curl -Lo panel.tar.gz https://github.com/pelican-dev/panel/releases/latest/download/panel.tar.gz
+tar -xzvf panel.tar.gz
+chmod -R 755 storage/* bootstrap/cache/
+composer install --no-dev --optimize-autoloader
+php artisan p:environment:setup
+php artisan p:environment:database
+php artisan p:environment:mail
+php artisan migrate --seed --force
+php artisan p:user:make
+sudo crontab -e && echo "* * * * * php /var/www/pelican/artisan schedule:run >> /dev/null 2>&1" | sudo crontab -u root -
+chown -R www-data:www-data /var/www/pelican/* 
 EOF
 } >> tmp.txt 2>&1
 
