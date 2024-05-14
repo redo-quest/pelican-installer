381463f0291ddfa33982b1081107fd71f2abf74a
v182 <142740981+v182@users.noreply.github.com>
2024-05-14 09:11:28 +0200

    fix

--- install.sh
+++ install.sh
@@ -517,7 +517,7 @@ MONITOR_PID=$!
 
 
 {
-cd /
+cd / <<EOF
 sudo apt install -y php8.2 php8.2-gd php8.2-mysql php8.2-mbstring php8.2-bcmath php8.2-xml php8.2-curl php8.2-zip php8.2-intl php8.2-sqlite3 php8.2-fpm php8.2-redis composer nginx xdotool
 xdotool key 'Return' | sudo add-apt-repository ppa:ondrej/php
 curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
